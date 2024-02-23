533b6e67db6d753ae7086f43ec6ef44ee9683587
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-24 00:26:38 +0100

    Basit test

---
+++ src/test/java/org/fugerit/java/demo/BasicApiTest.java
@@ -0,0 +1,46 @@
+package org.fugerit.java.demo;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+class BasicApiTest {
+    @Test
+    void testDocumentLoadAll() {
+        given()
+          .when().get("/document/load/all")
+          .then()
+             .statusCode(200)
+             .body(containsString("Silmarillion"));
+    }
+
+    @Test
+    void testExample01LoadAll() {
+        given()
+                .when().get("/example01/load/all")
+                .then()
+                .statusCode(200)
+                .body(containsString("field3"));
+    }
+
+    @Test
+    void testMappedtableLoadAll() {
+        given()
+                .when().get("/mappedtable/load/all")
+                .then()
+                .statusCode(200)
+                .body(containsString("Mapped"));
+    }
+    @Test
+    void testPersonLoadAll() {
+        given()
+                .when().get("/person/load/all")
+                .then()
+                .statusCode(200)
+                .body(containsString("Miriel"));
+    }
+
+}
