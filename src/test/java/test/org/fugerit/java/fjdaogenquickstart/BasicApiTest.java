b45e95230dbd777605c39267aeeff40ae3115748
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-28 15:24:36 +0100

    Added Sample Junit5ModelGenerator

--- src/test/java/test/org/fugerit/java/fjdaogenquickstart/BasicApiTest.java
+++ src/test/java/test/org/fugerit/java/fjdaogenquickstart/BasicApiTest.java
@@ -1,6 +1,7 @@
 package test.org.fugerit.java.fjdaogenquickstart;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
